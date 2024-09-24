93c28eeb3c33424bb76809255d869e085fb370cd
Umar <enggomarpu@gmail.com>
2024-09-24 08:30:28 +0500

    db context setup

---
+++ src/Modules/Catalog/Catalog/Data/CatalogDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Data.Configurations;
+using Catalog.Products.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Data
+{
+	public class CatalogDbContext : DbContext
+	{
+        public CatalogDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+		public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfiguration<Product>(new ProductConfiguration());
+		}
+	}
+}
