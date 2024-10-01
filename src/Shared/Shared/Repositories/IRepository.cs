7dcc7b4937cfc67ee4961a28c861f97ec72bfaad
Umar <enggomarpu@gmail.com>
2024-10-01 08:50:40 +0500

    changes

--- src/Shared/Shared/Repositories/IRepository.cs
+++ src/Shared/Shared/Repositories/IRepository.cs
@@ -6,40 +6,12 @@ using System.Linq.Expressions;
 
 namespace Shared.Repositories
 {
-	public interface IRepository<TEntity, TKey> where TEntity : Entity<TKey>
+	public interface IRepository<T> where T : Entity<Guid>
 	{
-		IUnitOfWork UnitOfWork { get; }
-
-		IQueryable<TEntity> GetQueryableSet();
-
-		Task AddOrUpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-
-		Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
-
-		Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
-
-		void Delete(TEntity entity);
-
-		Task<T> FirstOrDefaultAsync<T>(IQueryable<T> query);
-
-		Task<T> SingleOrDefaultAsync<T>(IQueryable<T> query);
-
-		Task<List<T>> ToListAsync<T>(IQueryable<T> query);
-
-		void BulkInsert(IEnumerable<TEntity> entities);
-
-		void BulkInsert(IEnumerable<TEntity> entities, Expression<Func<TEntity, object>> columnNamesSelector);
-
-		void BulkUpdate(IEnumerable<TEntity> entities, Expression<Func<TEntity, object>> columnNamesSelector);
-
-		void BulkMerge(IEnumerable<TEntity> entities, Expression<Func<TEntity, object>> idSelector, Expression<Func<TEntity, object>> updateColumnNamesSelector, Expression<Func<TEntity, object>> insertColumnNamesSelector);
-
-		void BulkDelete(IEnumerable<TEntity> entities);
-
-		public async Task<IEnumerable<T>> ListAllAsync()
-		{
-			return await _context.Set<T>().ToListAsync();
-		}
+		Task<T> GetByIdAsync(int id);
+		Task<IReadOnlyList<T>> ListAllAsync();
+		//Task<T> GetEntityWithSpec(ISpecification<T> spec);
+		//Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+		//Task<int> CountAsync(ISpecification<T> spec);
 	}
-
 }
